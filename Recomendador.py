8584f6a9ec8c263760f5a4f90ec7210f82d921d8
ErikFantomex <a217213318@unison.mx>
2022-11-29 17:20:35 -0700

    Ambiente virtual mas las herramientas de preprocesamiento

--- Recomendador.py
+++ Recomendador.py
@@ -1,6 +1,11 @@
 
 
 import pandas as pd
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+from fuzzywuzzy import fuzz
+
 import json
 
 import firebase_admin
